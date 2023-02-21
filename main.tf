b690f326d23872314c6e1713f5512d2f855bed73
root <root@ip-172-31-8-160.ap-south-1.compute.internal>
2023-02-21 15:52:27 +0000

    commits

---
+++ main.tf
@@ -0,0 +1,46 @@
+provider "aws" {
+region = "us-east-1"
+access_key = "AKIA5VT73D6PKCFC47M2"
+secret_key = "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"
+}
+
+resource "aws_instance" "one" {
+ami = "ami-01a4f99c4ac11b03c"
+instance_type = "t2.medium"
+  key_name        = "terraproject"
+  vpc_security_group_ids = [aws_security_group.three.id]
+  availability_zone = "us-east-1a"
+  user_data       = <<EOF
+#!/bin/bash
+sudo -i
+yum install httpd -y
+systemctl start httpd
+chkconfig httpd on
+echo "hai all this is my swiggy application created by terraform infrastructurte by raham sir server-1" > /var/www/html/index.html
+EOF
+tags = {
+Name = "swiggy-01"
+}
+}
+
+resource "aws_instance" "two" {
+ami = "ami-01a4f99c4ac11b03c"
+instance_type = "t2.medium"
+  key_name        = "terraproject"
+  vpc_security_group_ids = [aws_security_group.three.id]
+  availability_zone = "us-east-1b"
+  user_data       = <<EOF
+#!/bin/bash
+sudo -i
+yum install httpd -y
+systemctl start httpd
+chkconfig httpd on
+echo "hai all this is my swiggy application created by terraform infrastructurte by raham sir server-2" > /var/www/html/index.html
+EOF
+tags = {
+Name = "swiggy-02"
+}
+}
+
+
+
