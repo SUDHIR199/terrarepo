75323a6fcb466e58daac9b2702e42afe2c6e1dfb
Sudhir Mohanty <90685913+SUDHIR199@users.noreply.github.com>
2023-02-21 22:32:06 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-region = "us-east-1"
+region = "ap-south-1"
 access_key = "AKIA5VT73D6PKCFC47M2"
 secret_key = "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"
 }
@@ -9,7 +9,7 @@ ami = "ami-0dfcb1ef8550277af"
 instance_type = "t2.medium"
   key_name        = "terraproject"
   vpc_security_group_ids = [aws_security_group.three.id]
-  availability_zone = "us-east-1a"
+  availability_zone = "ap-south-1a"
   user_data       = <<EOF
 #!/bin/bash
 sudo -i
@@ -28,7 +28,7 @@ ami = "ami-01a4f99c4ac11b03c"
 instance_type = "t2.medium"
   key_name        = "terraproject"
   vpc_security_group_ids = [aws_security_group.three.id]
-  availability_zone = "us-east-1b"
+  availability_zone = "ap-south-1b"
   user_data       = <<EOF
 #!/bin/bash
 sudo -i
